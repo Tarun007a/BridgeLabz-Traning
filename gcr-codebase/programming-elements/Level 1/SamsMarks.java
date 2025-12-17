940e9ceef693125733a9f3ef9e92341ab069f618
Tarun007a <tarunchourey16@gmail.com>
2025-12-17 13:09:30 +0530

    [Tarun] : Added program to find Sam's average marks

---
+++ gcr-codebase/programming-elements/Level 1/SamsMarks.java
@@ -0,0 +1,17 @@
+public class SamsMarks{
+	public static void main(String[] args){
+		// Sam's math mark
+		int math = 94;
+		
+		// Sam's physics mark
+		int physics = 95;
+		
+		// Sam's chemistry mark
+		int chemistry = 96;
+		
+		// finding average marks
+		double average = (math + physics + chemistry) / 3.0;
+		
+		System.out.println("Sam’s average mark in PCM is : " + average	);
+	}
+}

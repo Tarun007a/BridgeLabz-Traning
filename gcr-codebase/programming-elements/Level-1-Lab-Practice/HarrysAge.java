04d0f6931d06004581b479e9e44b60d4cc2c273e
Tarun007a <tarunchourey16@gmail.com>
2025-12-17 12:16:55 +0530

    [Tarun] : Added program to find Harry's age

---
+++ gcr-codebase/programming-elements/Level-1-Lab-Practice/HarrysAge.java
@@ -0,0 +1,13 @@
+public class HarrysAge{
+	public static void main(String[] args){
+		// Harry's birth year 
+		int birthYear = 2000;	
+		
+		// Current year 
+		int currYear = 2024; 
+
+		// Finding Harry's age
+		int age = currYear - birthYear;		
+		System.out.println("Harry's age in 2024 is : " + age);
+	}
+}

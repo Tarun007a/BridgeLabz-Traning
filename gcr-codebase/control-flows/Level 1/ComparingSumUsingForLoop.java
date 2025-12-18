9bc273138fd8d3ec05b41fa7286aa8e9d8557395
Tarun007a <tarunchourey16@gmail.com>
2025-12-18 14:40:42 +0530

    [Tarun] : Added program for calculating and comparing sum of natural numbers using for loop

--- gcr-codebase/control-flows/Level 1/ComparingSumUsingForLoop.java
+++ gcr-codebase/control-flows/Level 1/ComparingSumUsingForLoop.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class SumOfNumbers{
+public class ComparingSumUsingForLoop{
 	public static void main(String[] args){
 		// creating scanner object and taking input
 		Scanner sc = new Scanner(System.in);
@@ -13,16 +13,14 @@ public class SumOfNumbers{
 		
 		// using formula
 		int sumUsingFormula = (n * (n+1))/2;
-		// using while loop
-		int sumUsingWhileLoop = 0;
-		int i = 1; 
-		while(i <= n){
-			sumUsingWhileLoop += i;
-			i++;
+		// using for loop
+		int sumUsingForLoop = 0;
+		for(int i = 1; i <= n; i++){
+			sumUsingForLoop += i;
 		}
 		
 		// comparing and printing the result
-		if(sumUsingFormula == sumUsingWhileLoop){
+		if(sumUsingFormula == sumUsingForLoop){
 			System.out.println("Sum is equal using while loop and formula, the sum is " + sumUsingFormula);
 		}
 		

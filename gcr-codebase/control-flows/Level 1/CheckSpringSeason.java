85377dc4939ee0534066fc28afb38f718d3832ae
Tarun007a <tarunchourey16@gmail.com>
2025-12-18 12:32:55 +0530

    [Tarun] : Added program for checking spring season

---
+++ gcr-codebase/control-flows/Level 1/CheckSpringSeason.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class CheckSpringSeason{
+	public static void main(String[] args){
+		// creating scanner object and taking input 
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter month : ");
+		int month = sc.nextInt();
+		System.out.print("Enter day : ");
+		int day = sc.nextInt();
+		
+		// Checking season and printing result
+		if((month == 3 && day >= 20 && day <= 31) || (month == 4 && day >= 1 && day <= 20)){
+			System.out.println("Its a Spring Season");
+		}
+		else{
+			System.out.println("Not a Spring Season");
+		}
+		
+		sc.close();
+	}
+}

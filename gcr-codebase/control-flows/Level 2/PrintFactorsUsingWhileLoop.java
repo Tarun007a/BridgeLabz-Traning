faff93b375596a99ed850b41df497e80c2ebff19
Tarun007a <tarunchourey16@gmail.com>
2025-12-18 16:24:48 +0530

    [Tarun] : Added program for finding factors using while loop

---
+++ gcr-codebase/control-flows/Level 2/PrintFactorsUsingWhileLoop.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class PrintFactorsUsingWhileLoop{
+    public static void main(String[] args) {
+        //Creating Scanner object and taking input
+        Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number : ");
+        int number = sc.nextInt();
+		int i = 1;
+
+        // using for loop to print result
+        while(i < number){
+            if(number%i == 0) System.out.println(i + " is a factor");
+			i++;
+        }
+		
+		sc.close();
+    }
+}

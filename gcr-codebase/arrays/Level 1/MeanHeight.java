c894da4979e670a3fd9d29c217292b30aa60e20c
Tarun007a <tarunchourey16@gmail.com>
2025-12-19 14:20:16 +0530

    [Tarun] : Added program for calculating mean height

---
+++ gcr-codebase/arrays/Level 1/MeanHeight.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class CheckNumbers{
+    public static void main(String[] args) {
+		// Creating Scanner Object and initializing array
+        Scanner sc = new Scanner(System.in);
+		double[] heights = new double[11];
+		double sum = 0;
+		
+		// taking input in array using a loop
+		for(int i = 0; i < heights.length; i++){
+			System.out.print("Enter height : ");
+			heights[i] = sc.nextInt();
+		}
+		
+		// Calculating sum
+		for(int i = 0; i < heights.length; i++){
+			sum += heights[i];
+		}
+		
+		// Calculating mean height and printing results
+		double meanHeight = sum / 11;
+		System.out.println("Mean height is : " + meanHeight);
+
+		sc.close();
+    }
+}

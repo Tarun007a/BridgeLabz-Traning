b6315beb58e6449255699c9a6105f6193bb281dd
Tarun007a <tarunchourey16@gmail.com>
2025-12-19 12:32:12 +0530

    [Tarun] : Added program for cheking voting eligibility

---
+++ gcr-codebase/arrays/Level 1/VoteEligibility.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class DayOfWeek {
+    public static void main(String[] args) {
+		// Creating Scanner Object and initializing array
+        Scanner sc = new Scanner(System.in);
+		int[] age = new int[10];
+		
+		// taking input in array using a loop
+		for(int i = 0; i < age.length; i++){
+			System.out.print("Enter age of student : ");
+			age[i] = sc.nextInt();
+		}
+		
+		// Checking age and printing eligibility using a for loop
+		for(int i = 0; i < age.length; i++){
+			if(age[i] <= 0) System.out.println("Invalid age");
+			else if(age[i] < 18) System.out.println("The student with the age " + age[i] + " cannot vote.");
+			else System.out.println("The student with the age " + age[i] + " can vote.");
+		}
+
+		sc.close();
+    }
+}

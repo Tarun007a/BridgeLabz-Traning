ef32e8943771fdd3ced6ecbe8ba5fddc941a10ed
Tarun007a <tarunchourey16@gmail.com>
2025-12-19 13:29:24 +0530

    [Tarun] : Added program for multiplication table from 6 to 9

---
+++ gcr-codebase/arrays/Level 1/MultiplicationTable6to9.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class MultiplicationTable{
+    public static void main(String[] args) {
+		// Creating Scanner Object, taking input and initializing array
+        Scanner sc = new Scanner(System.in);
+		System.out.print("Enter number : ");
+		int number = sc.nextInt();
+		int[] arr = new int[4];
+		
+		// Using a for loop to store the multiplication table in array
+		for(int i = 6; i < 10; i++){
+			arr[i-6] = (i) * number;
+		}
+		
+		// Printing the table using for loop
+		for(int i = 6; i < 10; i++){
+			System.out.println(number + " * " + (i) + " = " + arr[i-6]);
+		}
+
+		sc.close();
+    }
+}

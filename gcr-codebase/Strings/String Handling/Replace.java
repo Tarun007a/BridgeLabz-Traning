be6c7749190d747f6194a3e3ff54d8ca3d394cf5
Tarun007a <tarunchourey16@gmail.com>
2025-12-24 15:12:02 +0530

    [Tarun] : Updated code

--- gcr-codebase/Strings/String Handling/Replace.java
+++ gcr-codebase/Strings/String Handling/Replace.java
@@ -8,11 +8,14 @@ public class Replace{
 		String s = sc.nextLine();
 		System.out.print("Enter word to remove : ");
 		String remove = sc.next();
+		System.out.print("Enter new word : ");
+		String newWord = sc.next();
 		String result = "";
 		
 		// using split to split in word and the creating result
 		for(String str : s.split(" ")){
 			if(!str.equals(remove)) result += " " + str;
+			else result += " " + newWord;
 		}
 		
 		System.out.println("The resultatn string is " + result);

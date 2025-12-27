6317ad59997c225bb201e1e35858e61b070143dd
Tarun007a <tarunchourey16@gmail.com>
2025-12-27 16:31:49 +0530

    [Tarun] : Updated program for course management

--- java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructor/level1/CourseManagement.java
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructor/level1/CourseManagement.java
@@ -6,21 +6,25 @@ public class CourseManagement {
 	private int fee;
 	private static String instituteName = "Institute of Science";
 	
+	// constructor
 	public CourseManagement(String courseName, int duration, int fee) {
 		this.courseName = courseName;
 		this.duration = duration;
 		this.fee = fee;
 	}
 	
+	// display course details
 	public void displayCourseDetails() {
 		System.out.println(courseName + " " + duration + " " + fee);
 	}
 	
+	// update institute name
 	public void updateInstituteName(String instituteName) {
 		this.instituteName = instituteName;
 	}
 	
 	public static void main(String[] args) {
+		//  create object and call functions
 		CourseManagement course = new CourseManagement("b.tech", 4, 1000000);
 		course.displayCourseDetails();
 		course.updateInstituteName("New institute name");

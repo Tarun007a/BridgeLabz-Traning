08c3fb2dd245f1fd866c9ef6a8fa6307d86791a8
Tarun007a <tarunchourey16@gmail.com>
2025-12-27 16:58:02 +0530

    [Tarun] : Updated program for Book Library System

--- java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructor/level1/EBook.java
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructor/level1/EBook.java
@@ -2,6 +2,7 @@ package com.constructor.level1;
 
 public class EBook extends Book1{
 	public static void main(String[] args) {
+		// creating object and accessing the properties of super class
 		EBook book = new EBook();
 		book.setAuthor("Authro1");
 		System.out.println(book.getAuthor());

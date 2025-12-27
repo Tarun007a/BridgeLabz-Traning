497bdb2e2bf882955cf74f1f7292bd7214bf3ac6
Tarun007a <tarunchourey16@gmail.com>
2025-12-27 16:50:12 +0530

    [Tarun] : Updated program for University Management System

--- java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructor/level1/PostgraduateStudent.java
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructor/level1/PostgraduateStudent.java
@@ -2,6 +2,7 @@ package com.constructor.level1;
 
 public class PostgraduateStudent extends Student{
 	public static void main(String[] args) {
+		// creating object and accessing protected member
 		PostgraduateStudent student1 = new PostgraduateStudent();
 		student1.name = "Student 1";
 	}

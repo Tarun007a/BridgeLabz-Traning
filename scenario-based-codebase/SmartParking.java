24b97f917b379faa94581e05a40a62749e2ba803
Tarun007a <tarunchourey16@gmail.com>
2025-12-29 12:14:23 +0530

    [Tarun] : Updated program for smart parking system

--- scenario-based-codebase/SmartParking.java
+++ scenario-based-codebase/SmartParking.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 public class SmartParking{
+	// method to park vehicle
 	private static int parkInSlot(int idx, int size){
 		if(idx == size) {
 			System.out.println("The parking is full");
@@ -12,7 +13,7 @@ public class SmartParking{
 		return idx+1;
 	}
 	
-	
+	// method to print occupancy of slots
 	private static void showOccupancy(int idx, int size){
 		for(int i = 0; i < size; i++){
 			if(i >= idx) System.out.println("slot " + (i+1) + " is empty");
@@ -24,14 +25,19 @@ public class SmartParking{
 		// creating scanner object
 		Scanner sc = new Scanner(System.in);
 		
+		// taking input
 		System.out.println("Welcome to smart parking");
 		System.out.print("Enter the maximum cars to be parked : ");
 		int size = sc.nextInt();
 		int idx = 0;
-				
+		
+		// using infinite while loop		
 		while(true){
+			// taking user input
 			System.out.print("Enter 1 for park, 2 for show occupancy, 3 for exit : ");
 			int choice = sc.nextInt();
+			
+			// using switch case
 			switch(choice){
 				case 1 : 
 					idx = parkInSlot(idx, size);

cb738017023ed3b193885754511a7bbfb230348a
Tarun007a <tarunchourey16@gmail.com>
2025-12-29 15:17:41 +0530

    [Tarun] : Added program to calculate discount

---
+++ scenario-based-codebase/Discount.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class GuessNumber{
+	public static void main(String[] args){
+		// creating scanner class object and taking user input
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Welcome to discount dashboard");
+		System.out.println("If you shop below 1000 you will get 2% discount");
+		System.out.println("If you shop above 1000 you will get 5% discount");
+		System.out.println("Enter number of products");
+		int n = sc.nextInt();
+		int sum  = 0;
+		
+		// calculating total sum
+		for(int i = 0; i < n; i++){
+			System.out.print("Enter price of prodcut : ");
+			sum += sc.nextInt();
+		}
+		
+		if(sum >= 1000){
+			System.out.println("Total cost " + sum);
+			System.out.println("Discount " + (sum/20));
+			System.out.println("Discounted cost " + (sum - ((sum/20))));
+		}
+		else{
+			System.out.println("Total cost " + sum);
+			System.out.println("Discount " + (sum/50));
+			System.out.println("Discounted cost " + (sum - ((sum/50))));
+		}
+	}
+}

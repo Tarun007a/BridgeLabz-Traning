da62dedec00b0388e8d5c6854f8c68499fd0c329
Tarun007a <tarunchourey16@gmail.com>
2025-12-29 14:59:49 +0530

    [Tarun] : Updated Guess number game program

--- scenario-based-codebase/GuessNumber.java
+++ scenario-based-codebase/GuessNumber.java
@@ -2,12 +2,15 @@ import java.util.Scanner;
 
 public class GuessNumber{
 	public static void main(String[] args){
+		// creating scanner class object
 		Scanner sc = new Scanner(System.in);
 		int lo = 0;
 		int hi = 100;
+		// finding the random number
 		int number = (int) (Math.random() * 100) + 1;
 		int chances = 5;
 		
+		// using do while loop to take user number and checking using if else 
 		do{
 			System.out.print("Enter the number you gussed : ");
 			int choice = sc.nextInt();
@@ -19,6 +22,8 @@ public class GuessNumber{
 			else System.out.println("Too high");
 			chances--;
 		}while(chances > 0);
+		
+		// printing the correct number
 		System.out.println("The correct number is : " + number);
 	}
 }

107e225c5bc8acee52921d4f98b778b352fd0ccd
Tarun007a <tarunchourey16@gmail.com>
2026-01-01 13:03:52 +0530

    [Tarun] : Updated program Educational Course Hierarchy

--- java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/multilevelinheritance/coursehierarchy/DriverClass.java
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/multilevelinheritance/coursehierarchy/DriverClass.java
@@ -1,5 +1,11 @@
 package com.inheritance.multilevelinheritance.coursehierarchy;
 
 public class DriverClass {
+	public static void main(String[] args) {
 
+		// creating object
+        PaidOnlineCourse c1 = new PaidOnlineCourse("Java", 1, "platform1", true, 1000, 1000);
+
+        c1.displayDetails();
+    }
 }

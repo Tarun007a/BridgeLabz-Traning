f91af5c0e50bbdb724ad5b523695d95654ff2a7d
Tarun007a <tarunchourey16@gmail.com>
2026-01-06 21:22:05 +0530

    [Tarun] : Added code to read data through a file

---
+++ java-dsa-practice/gcr-codebase/dsapractice/src/com/searchingalgorithms/filereader/ReadFile.java
@@ -0,0 +1,21 @@
+package com.searchingalgorithms.filereader;
+
+import java.io.*;
+
+public class ReadFile {
+    public static void main(String[] args) {
+
+        try {
+        	BufferedReader br = new BufferedReader(new FileReader("input.txt"));
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+
+        } 
+        catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}

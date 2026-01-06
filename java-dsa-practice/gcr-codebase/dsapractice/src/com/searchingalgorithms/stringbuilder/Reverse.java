2f9d25eeeb411cf26e2a607696086eb206497cfc
Tarun007a <tarunchourey16@gmail.com>
2026-01-06 15:49:57 +0530

    [Tarun] : Added program to reverse using string builder

---
+++ java-dsa-practice/gcr-codebase/dsapractice/src/com/searchingalgorithms/stringbuilder/Reverse.java
@@ -0,0 +1,8 @@
+package com.searchingalgorithms.stringbuilder;
+
+public class Reverse {
+	public static void main(String[] agrs) {
+		StringBuilder sb = new StringBuilder("hello");
+		System.out.println(sb.reverse());
+	}
+}

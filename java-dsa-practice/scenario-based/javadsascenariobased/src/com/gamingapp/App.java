df25f45483a3df6121cf01b6d93dab030447cba1
Tarun007a <tarunchourey16@gmail.com>
2026-01-23 12:20:29 +0530

    [Tarun] : Updated program for Gaming App - Real-Time Leaderboard

--- java-dsa-practice/scenario-based/javadsascenariobased/src/com/gamingapp/App.java
+++ java-dsa-practice/scenario-based/javadsascenariobased/src/com/gamingapp/App.java
@@ -36,6 +36,57 @@ public class App {
 
         return x;
     }
+    
+    private static Node minValueNode(Node node) {
+        Node current = node;
+        while (current.left != null) current = current.left;
+        return current;
+    }
+    
+    private static Node delete(Node root, int points) {
+        if (root == null) return null;
+
+        if (points < root.points) root.left = delete(root.left, points);
+        else if (points > root.points) root.right = delete(root.right, points);
+        else {
+            if (root.left == null && root.right == null) return null;
+
+            if (root.left == null) return root.right;
+
+            if (root.right == null) return root.left;
+
+            Node successor = minValueNode(root.right);
+            root.points = successor.points;
+            root.name = successor.name;
+            root.right = delete(root.right, successor.points);
+        }
+
+        root.height = 1 + Math.max(getHeight(root.left), getHeight(root.right));
+
+        int balance = getBalance(root);
+
+        if (balance > 1 && getBalance(root.left) >= 0) {
+        	return rightRotate(root);
+        }
+
+        if (balance > 1 && getBalance(root.left) < 0) {
+            root.left = leftRotate(root.left);
+            return rightRotate(root);
+        }
+
+        if (balance < -1 && getBalance(root.right) <= 0) {
+        	return leftRotate(root);
+        }
+
+        if (balance < -1 && getBalance(root.right) > 0) {
+            root.right = rightRotate(root.right);
+            return leftRotate(root);
+        }
+
+        return root;
+    }
+
+
 
     private static Node insertNode(Node root, int points, String name) {
         if (root == null) return new Node(name, points);
@@ -67,6 +118,11 @@ public class App {
 
         return root;
     }
+    
+    private static Node update(Node root, int oldPoints, String newName, int newPoints) {
+    	root = delete(root, oldPoints);
+    	return insertNode(root, newPoints, newName);
+    }
 
     private static void getTopTen(Node root, ArrayList<Node> topPlayers) {
         if (root == null || topPlayers.size() >= 10)
@@ -88,7 +144,16 @@ public class App {
 
         ArrayList<Node> list = new ArrayList<>();
         getTopTen(root, list);
-
+        System.out.println(list);
+        
+        root = delete(root, 900);
+        list.clear();
+        getTopTen(root, list);
+        System.out.println(list);
+        
+        update(root, 1200, "Updated player 1", 1600);
+        list.clear();
+        getTopTen(root, list);
         System.out.println(list);
     }
 }

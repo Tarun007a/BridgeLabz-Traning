e93577bb4df8548e8cb9dfe59840079e6fc9d14a
Tarun007a <tarunchourey16@gmail.com>
2026-01-26 16:05:03 +0530

    [Tarun] : Added program to Validate a Social Security Number (SSN)

--- java-collections-practice/gcr-codebase/javacollectionspractice/src/com/regax/FindRepeatingWords.java
+++ java-collections-practice/gcr-codebase/javacollectionspractice/src/com/regax/FindRepeatingWords.java
@@ -4,9 +4,8 @@ import java.util.regex.*;
 
 public class FindRepeatingWords {
     public static void main(String[] args) {
-
         String text = "This is is a repeated repeated word test.";
-        Pattern pattern = Pattern.compile("\\b(\\w+)\\s+\\1\\b", Pattern.CASE_INSENSITIVE);
+        Pattern pattern = Pattern.compile("\\b(\\w+)\\s+\\1\\b");
         Matcher matcher = pattern.matcher(text);
 
         while (matcher.find()) {

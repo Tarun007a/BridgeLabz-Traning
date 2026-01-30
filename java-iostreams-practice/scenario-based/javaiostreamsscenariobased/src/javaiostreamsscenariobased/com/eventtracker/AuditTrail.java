c42cfeb90866c839541bf1c55182be112c4ed8a5
Tarun007a <tarunchourey16@gmail.com>
2026-01-30 22:44:05 +0530

    [Tarun] : Added program for EventTracker – Auto Audit System (Annotations + Reflection + JSON)

---
+++ java-iostreams-practice/scenario-based/javaiostreamsscenariobased/src/javaiostreamsscenariobased/com/eventtracker/AuditTrail.java
@@ -0,0 +1,9 @@
+package javaiostreamsscenariobased.com.eventtracker;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface AuditTrail {
+    String action();
+}

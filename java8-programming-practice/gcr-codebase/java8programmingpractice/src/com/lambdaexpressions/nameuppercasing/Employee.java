7db7936e14ae4e8be848b3d81cf0ed7f362bc447
Tarun007a <tarunchourey16@gmail.com>
2026-01-31 15:53:25 +0530

    [Tarun] : Added program for Name Uppercasing

---
+++ java8-programming-practice/gcr-codebase/java8programmingpractice/src/com/lambdaexpressions/nameuppercasing/Employee.java
@@ -0,0 +1,13 @@
+package com.lambdaexpressions.nameuppercasing;
+
+public class Employee {
+	String name;
+	
+	public Employee(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+}

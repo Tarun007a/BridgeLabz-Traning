a7f1447e999222eaba9f24b7562f8a1c5454f8ea
Tarun007a <tarunchourey16@gmail.com>
2026-02-01 15:49:31 +0530

    [Tarun] : Updated program for String Length Checker

--- java8-programming-practice/gcr-codebase/java8programmingpractice/src/com/functionalinterfaces/StringLengthChecker.java
+++ java8-programming-practice/gcr-codebase/java8programmingpractice/src/com/functionalinterfaces/StringLengthChecker.java
@@ -1,14 +1,10 @@
 package com.functionalinterfaces;
 
-import java.util.List;
 import java.util.function.Function;
 
 public class StringLengthChecker {
-	public static void main(String[] args) {
-		List<String> messages = List.of("Message 1", "New lengthy message", "short message");
-		
-		Function<String, Integer> function = s -> s.length();
-		
-		messages.stream().map(function).filter(null)
-	}
+    public static void main(String[] args) {
+        Function<String, Integer> length = String::length;
+        System.out.println(length.apply("Message Length"));
+    }
 }
